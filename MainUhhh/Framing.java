9d2d39db76bea3b34cf02f210b7adb2edf7b9675
SafiZ <tcabato@addu.edu.ph>
2025-08-04 08:47:42 +0800

    ion gotta work

--- MainUhhh/Framing.java
+++ MainUhhh/Framing.java
@@ -4,15 +4,21 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-public class Framing extends JFrame {
+public class Framing extends JFrame implements ActionListener {
+    JButton button;
+    JPanel centerPanel;
+
     public Framing() {
         // JFrame frame = new JFrame(); Generating another frame technically
 
@@ -23,6 +29,7 @@ public class Framing extends JFrame {
             this.setResizable(false);
 
             ImageIcon icon = new ImageIcon("C:\\Users\\tcabato\\Downloads\\Phrolova-icon.png");
+            ImageIcon buttonIcon = new ImageIcon(icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
             this.setIconImage(icon.getImage());
 
             this.getContentPane().setBackground(new Color(108, 124, 147));
@@ -52,6 +59,54 @@ public class Framing extends JFrame {
                 label2.setHorizontalAlignment(JLabel.CENTER);
                 label2.setPreferredSize(new Dimension(250,50));
                 
+        // Button
+            button = new JButton("Click");
+            button.setPreferredSize(new Dimension(200,50));
+            button.setFocusable(false);
+            button.addActionListener(e -> {
+                if(centerPanel.isVisible()) { 
+                    centerPanel.setVisible(false);
+                } else { 
+                    centerPanel.setVisible(true);
+                }
+            });
+            button.setIcon(buttonIcon);
+            button.setHorizontalTextPosition(JButton.CENTER);
+            button.setVerticalTextPosition(JButton.BOTTOM);
+            // button.setIconTextGap(-5);
+
+            centerPanel = new JPanel();
+            centerPanel.setPreferredSize(new Dimension(300,300));
+            centerPanel.setBackground(Color.RED);
+            centerPanel.setVisible(false);
+
+            // Other Buttons
+                JButton button2 = new JButton("");
+                    button2.setBackground(Color.PINK);
+                    button2.addActionListener(e -> {
+                        centerPanel.setBackground(Color.PINK);
+                    });
+                JButton button3 = new JButton("");
+                    button3.setBackground(Color.BLUE);
+                    button3.addActionListener(e -> {
+                        centerPanel.setBackground(Color.BLUE);
+                    });
+                JButton button4 = new JButton("");
+                    button4.setBackground(Color.GREEN);
+                    button4.addActionListener(e -> {
+                        centerPanel.setBackground(Color.GREEN); 
+                    });
+                JButton button5 = new JButton("");
+                    button5.setBackground(Color.YELLOW);
+                    button5.addActionListener(e -> {
+                        centerPanel.setBackground(Color.YELLOW);
+                    });
+                JButton button6 = new JButton("");
+                    button6.setBackground(Color.BLACK);
+                    button6.addActionListener(e -> {
+                        centerPanel.setBackground(Color.BLACK);
+                    });
+        
         // Border
             Border border = BorderFactory.createLineBorder(Color.BLUE, 6, true);
             // Border frameborder = BorderFactory.createSoftBevelBorder(5);
@@ -71,6 +126,20 @@ public class Framing extends JFrame {
             JPanel panel9 = new JPanel();
             JPanel panel10 = new JPanel();
             JPanel panel11 = new JPanel();
+
+            JPanel panel12 = new JPanel();
+            JPanel panel13 = new JPanel();
+            JPanel panel14 = new JPanel();
+            JPanel panel15 = new JPanel();
+            JPanel panel16 = new JPanel();
+            JPanel panel17 = new JPanel();
+
+            JPanel panel18 = new JPanel();
+            JPanel panel19 = new JPanel();
+            JPanel panel20 = new JPanel();
+            JPanel panel21 = new JPanel();
+            JPanel panel22 = new JPanel();
+            JPanel panel23 = new JPanel();
             // Backgrounds
                 panel1.setBackground(Color.RED);
                 panel2.setBackground(Color.GREEN);
@@ -84,17 +153,31 @@ public class Framing extends JFrame {
                 panel9.setBackground(Color.YELLOW);
                 panel10.setBackground(Color.ORANGE);
                 panel11.setBackground(Color.PINK);
+
+                panel12.setBackground(Color.RED);
+                panel13.setBackground(Color.BLACK);
+                panel14.setBackground(Color.BLUE);
+                panel15.setBackground(Color.YELLOW);
+                panel16.setBackground(Color.ORANGE);
+                panel17.setBackground(Color.PINK);
+
+                panel18.setBackground(Color.RED);
+                panel19.setBackground(Color.BLACK);
+                panel20.setBackground(Color.BLUE);
+                panel21.setBackground(Color.YELLOW);
+                panel22.setBackground(Color.ORANGE);
+                panel23.setBackground(Color.PINK);
             // Preferred Size
                 panel1.setPreferredSize(new Dimension(100,50));
                 panel2.setPreferredSize(new Dimension(50,100));
                 panel3.setPreferredSize(new Dimension(50,100));
                 panel4.setPreferredSize(new Dimension(100,50));
+                panel5.setPreferredSize(new Dimension(100,50));
                 
                 panel6.setLayout(new BorderLayout());
                 panel7.setLayout(new BorderLayout());
                     panel7.setPreferredSize(new Dimension(100,50));
-                panel8.setLayout(new BorderLayout());
-                    panel8.setPreferredSize(new Dimension(100,50));
+                panel8.setPreferredSize(new Dimension(100,50));
                 panel9.setLayout(new BorderLayout());
                 panel10.setLayout(new BorderLayout());
                     panel10.setPreferredSize(new Dimension(100,50));
@@ -105,10 +188,12 @@ public class Framing extends JFrame {
                 add(panel3, BorderLayout.WEST);
                 add(panel4, BorderLayout.SOUTH);
                 add(panel5, BorderLayout.CENTER);
+                    panel5.add(centerPanel);
             
                 panel1.setLayout(new BorderLayout());
                 panel1.add(panel7, BorderLayout.WEST);
                 panel1.add(panel8, BorderLayout.CENTER);
+                    panel8.add(button); 
                 panel1.add(panel10, BorderLayout.EAST);
 
                 panel4.setLayout(new BorderLayout());
@@ -118,7 +203,53 @@ public class Framing extends JFrame {
                 panel4.add(panel6, BorderLayout.WEST);
                 panel4.add(panel9, BorderLayout.CENTER);
                 panel4.add(panel11, BorderLayout.EAST);
+
+                panel12.setPreferredSize(new Dimension(30,30));
+                panel13.setPreferredSize(new Dimension(30,30));
+                panel14.setPreferredSize(new Dimension(30,30));
+                panel15.setPreferredSize(new Dimension(30,30));
+                panel16.setPreferredSize(new Dimension(30,30));
+
+                // Layouts
+                    panel12.setLayout(new BorderLayout());
+                    panel13.setLayout(new BorderLayout());
+                    panel14.setLayout(new BorderLayout());
+                    panel15.setLayout(new BorderLayout());
+                    panel16.setLayout(new BorderLayout());
+                panel2.add(panel12);
+                    panel12.add(button2);
+                panel2.add(panel13);
+                    panel13.add(button3);
+                panel2.add(panel14);
+                    panel14.add(button4);
+                panel2.add(panel15);
+                    panel15.add(button5);
+                panel2.add(panel16);
+                    panel16.add(button6);
+                
+                panel18.setPreferredSize(new Dimension(30,50));
+                panel19.setPreferredSize(new Dimension(30,50));
+                panel20.setPreferredSize(new Dimension(30,50));
+                panel21.setPreferredSize(new Dimension(30,50));
+                panel22.setPreferredSize(new Dimension(30,50));
+                panel23.setPreferredSize(new Dimension(30,50));
+                panel3.add(panel18, BorderLayout.WEST);
+                panel3.add(panel19, BorderLayout.CENTER);
+                panel3.add(panel20, BorderLayout.EAST);
+
+                panel3.add(panel21, BorderLayout.WEST);
+                panel3.add(panel22, BorderLayout.CENTER);
+                panel3.add(panel23, BorderLayout.EAST);
                 
                 this.setVisible(true);
     }
+    
+/*    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(centerPanel.isVisible()) { 
+            centerPanel.setVisible(false);
+        } else { 
+            centerPanel.setVisible(true);
+        }
+    } Ignore this for now lol */
 }
